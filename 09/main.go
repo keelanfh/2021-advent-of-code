30b67578c19bbaf96d5f5615099527d034f197f4
keelanfh <info@keelanfh.com>
2021-12-18 12:46:56 +0000

    day 9 part 2

--- 09/main.go
+++ 09/main.go
@@ -2,19 +2,65 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 
 	"github.com/keelanfh/2021-advent-of-code/utils"
 )
 
+// Set up length and width as global variables so we can do bounds checking easily
+var length, width int
+var basinSizes []int
+var lowPoints, diffs [][2]int
+var grid [][]int
+var inBasinGrid [][]inBasinResult
+
+type inBasinResult struct {
+	found       bool
+	inBasin     bool
+	basinNumber int
+}
+
+func outOfBounds(i, j int) bool {
+	return (i < 0 || i >= length || j < 0 || j >= width)
+}
+
+func addToBasin(i, j, num int) (bool, int) {
+	result := inBasinGrid[i][j]
+	if result.found {
+		return result.inBasin, result.basinNumber
+	}
+	var inBasin bool
+	var basinNumberRes int
+
+	for _, diff := range diffs {
+		i_diff, j_diff := i+diff[0], j+diff[1]
+		if outOfBounds(i_diff, j_diff) {
+			continue
+		}
+
+		if grid[i_diff][j_diff] < num {
+			for basinNumber, lowPoint := range lowPoints {
+				if lowPoint[0] == i_diff && lowPoint[1] == j_diff {
+					inBasinGrid[i][j] = inBasinResult{found: true, inBasin: true, basinNumber: basinNumber}
+					return true, basinNumber
+				} else {
+					// recurse the function
+					inBasin, basinNumberRes = addToBasin(i_diff, j_diff, grid[i_diff][j_diff])
+					inBasinGrid[i][j] = inBasinResult{found: true, inBasin: inBasin, basinNumber: basinNumberRes}
+				}
+			}
+		}
+	}
+	// inBasin will remain false if it's never been set to true
+	// I'm still not 100% sure how this works
+	// I wrote it, and I kinda get the logic, but still...
+	return inBasin, basinNumberRes
+}
+
 func main() {
 
 	scanner := utils.ReadFileLines("09/input.txt")
-
-	var grid [][]int
-	// for _, line := range grid {
-	// 	line = make([]int, 0)
-	// }
 
 	for scanner.Scan() {
 		var list []int
@@ -28,13 +74,22 @@ func main() {
 
 	var risk int
 
+	length = len(grid)
+	width = len(grid[0])
+
+	inBasinGrid = make([][]inBasinResult, length)
+	for i := 0; i < length; i++ {
+		inBasinGrid[i] = make([]inBasinResult, width)
+	}
+
+	diffs = [][2]int{{-1, 0}, {0, 1}, {1, 0}, {0, -1}}
+
 	for i, line := range grid {
 		for j, num := range line {
 			low := true
-			for _, diff := range [][2]int{{-1, 0}, {0, 1}, {1, 0}, {0, -1}} {
+			for _, diff := range diffs {
 				i_diff, j_diff := i+diff[0], j+diff[1]
-				if i_diff < 0 || i_diff >= len(grid) || j_diff < 0 || j_diff >= len(line) {
-					// out of bounds of array
+				if outOfBounds(i_diff, j_diff) {
 					continue
 				}
 				if grid[i_diff][j_diff] <= num {
@@ -44,10 +99,34 @@ func main() {
 			}
 			if low {
 				risk += num + 1
+				lowPoints = append(lowPoints, [2]int{i, j})
 			}
 		}
 	}
 
 	fmt.Println(risk)
 
+	// Part 2
+
+	basinSizes = make([]int, len(lowPoints))
+
+	for i := range basinSizes {
+		basinSizes[i]++
+	}
+
+	for i, line := range grid {
+		for j, num := range line {
+			if num == 9 {
+				continue
+			}
+			toAdd, pointNumber := addToBasin(i, j, num)
+			if toAdd {
+				basinSizes[pointNumber]++
+			}
+		}
+	}
+
+	sort.Ints(basinSizes)
+	lastIndex := len(basinSizes)
+	fmt.Println(basinSizes[lastIndex-1] * basinSizes[lastIndex-2] * basinSizes[lastIndex-3])
 }
